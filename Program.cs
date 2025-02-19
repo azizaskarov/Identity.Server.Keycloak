7bbf0fc54867763ce9910b966252583ce2e09685
Azizbek Askarov <105011522+azizaskarov@users.noreply.github.com>
2025-02-19 17:39:31 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,14 +2,11 @@ using Identity.Server.Keycloak.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 🔹 Keycloak sozlamalarini yuklash
 builder.Services.Configure<KeycloakSettings>(builder.Configuration.GetSection("Keycloak"));
 
-// 🔹 HttpClient va Keycloak xizmatini ro‘yxatdan o‘tkazish
 builder.Services.AddHttpClient<KeycloakService>();
 builder.Services.AddScoped<KeycloakService>();
 
-// 🔹 CORS ruxsat berish
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -20,26 +17,23 @@ builder.Services.AddCors(options =>
     });
 });
 
-// 🔹 Controllers va Swagger qo‘shish
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 🔹 HTTPS majburiy emas (local dev uchun)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// 🔹 CORS yoqish
 app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication(); // 🔹 Authentifikatsiya ishlatish
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
